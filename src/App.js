bca8b3aebfa3c4d8639a454627dc3cb3ee090c28
hishambokhari <hish.b92@gmail.com>
2020-06-30 23:54:36 +0500

    dynamic input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
+import Validation from './ValidationComponent/ValidationComponent'
 
 class App extends Component {
   state = {
@@ -19,6 +20,7 @@ class App extends Component {
         onChange={this.inputChangedHandler} 
         value={this.state.userInput}/>
         <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}/>
       </div>
     );
   }
